517e3aa399c33e9501ed635e74ae65c8f8710aac
kazuto masui <kazutooo.110@gmail.com>
2017-07-10 09:01:44 +0000

    atom

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,6 +12,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contacts_params)
    if @contact.save
      redirect_to root_path, notice: "お問い合わせが完了しました！"
+     NoticeMailer.sendmail_contacts(@contact).deliver
    else
       render 'new'
    end
--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -3,12 +3,18 @@ class NoticeMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.notice_mailer.sendmail_blog.subject
+  #   en.notice_mailer.sendmail_contacts.subject
   #
+  
   def sendmail_blog(blog)
     @blog = blog
-
     mail to: "kazutooo.110@gmail.com",
     subject: '【Achieve】ブログが投稿されました'
+  end
+  
+  def sendmail_contacts(contact)
+    @contact = contact
+    mail to: @contact.email,
+    subject: '【Achieve】お問い合わせが完了しました'
   end
 end
