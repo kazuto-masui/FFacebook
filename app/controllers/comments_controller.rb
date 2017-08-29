e54f8fd330d49ee32a10bfee18dd8231ca166adf
桝井一十 <masuikazuto@masuiichijuunoMacBook-Air.local>
2017-08-29 20:33:08 +0900

    DIVE19-2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,22 @@
 class CommentsController < ApplicationController
-  # コメントを保存、投稿するためのアクションです。
+
   def create
-    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
     @comment = current_user.comments.build(comment_params)
     @blog = @comment.blog
-    # クライアント要求に応じてフォーマットを変更
+    @notification = @comment.notifications.build(user_id: @blog.user.id )
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
         format.js { render :index }
-      else
+        unless @comment.blog.user_id == current_user.id
+         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
+          message: 'あなたの作成したブログにコメントが付きました'
+         })
+        end
+         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
+          unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count
+         })
+        else
         format.html { redirect_to blog_path(@blog), notice: 'コメント投稿に失敗しました。' }
         format.js { render :index }
       end
