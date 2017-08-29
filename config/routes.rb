085f4205255ce460a55032db067cf214c6e7d3c7
桝井一十 <masuikazuto@masuiichijuunoMacBook-Air.local>
2017-08-29 18:32:24 +0900

    DIVE19

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,38 @@
 Rails.application.routes.draw do
 
-  get 'relationships/create'
-
-  get 'relationships/destroy'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users,controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  resources :blogs do
+
+resources :blogs do
    resources :comments
    post :confirm, on: :collection
+end
+
+resources :contacts,only: [:new, :create] do
+  collection do
+   post :confirm
   end
+end
 
-  resources :contacts,only: [:new, :create]do
-   collection do
-    post :confirm
-   end
-  end
+resources :users, only: [:index, :show]
 
-  resources :users, only: [:index, :show]
+resources :relationships, only: [:create, :destroy]
 
-  resources :relationships, only: [:create, :destroy]
+resources :conversations do
+   resources :messages
+end
 
-  root 'top#index'
+root 'top#index'
 
   if Rails.env.development?
    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  resources :poems, only: [:index, :show]
-
-
+resources :poems, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
