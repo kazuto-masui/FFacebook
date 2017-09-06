96297f7f1dee0364aef9b312fb05e1e5c4b1815d
桝井一十 <masuikazuto@masuiichijuunoMacBook-Air.local>
2017-09-07 06:37:51 +0900

    メール機能 production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://13.113.255.158' }
+  config.action_mailer.default_url_options = { host: 'http://13.114.237.133' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170829102834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "avatar"
   end
 
   create_table "comments", force: :cascade do |t|
